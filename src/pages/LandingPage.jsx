1df1a5a43c1a3ede0becfa80de71eff791fc3e84
Were Samson Bruno <werefast2000@gmail.com>
2024-06-21 18:42:08 +0000

    Create Landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import Helmet from 'react-helmet';
+import Header from '../components/Header';
+import { Text, Flex, Box, Image, Button, Heading, Container, Textarea, AbsoluteCenter } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+
+const LandingPage = () => {
+    return (
+      <>
+        <Helmet>
+          <title>Spives - Home</title>
+          <meta name="description" content="Spives - Home" />
+        </Helmet>
+  
+        <Header />
+      </>
+    );
+  }
+  
+  export default LandingPage;
