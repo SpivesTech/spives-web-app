b252b2676976d87e551a7b100d67f3cb39e3853c
Were Samson Bruno <werefast2000@gmail.com>
2024-06-21 18:40:04 +0000

    rename App.js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
+import { BrowserRouter as Router } from 'react-router-dom';
+import ProjectRoutes from './Routes'; 
+import { ChakraProvider, theme, Box,
   Text,
   Link,
   VStack,
   Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+  Grid, } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
@@ -18,10 +16,11 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
+          
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Edit <Code fontSize="xl">src/App.jsx</Code> and save to reload.
             </Text>
             <Link
               color="teal.500"
