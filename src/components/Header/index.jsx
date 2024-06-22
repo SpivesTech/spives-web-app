0584107da23f5ed031019c85b834c61be84fea09
Were Samson Bruno <werefast2000@gmail.com>
2024-06-22 13:52:33 +0000

    Create Header section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Button, Heading, Text, Flex, List, ListItem, Image, Link, UnorderedList } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
 
 export default function Header({ ...props }) {
     return (
@@ -14,34 +16,55 @@ export default function Header({ ...props }) {
         flexDirection={{ md: "row", base: "column" }}
         as="header"
       >
-        <Image src="images/img_header_logo.png" h="28px" mb="6px" w="128px" fit="contain" />
+        <RouterLink to="/">
+          <Image src="/images/spives.png" h="28px" mb="6px" w="128px" fit="contain" alt="Spives Logo" />
+        </RouterLink>
         <Flex mb="6px" mr="24px" justifyContent="center" alignItems="center">
           <UnorderedList styleType="none" mb="4px !important" gap="34px" display="flex" flexWrap="wrap">
             <ListItem>
-              <Link href="#">
+              <Link as={RouterLink} to="/features">
                 <Text size="textlg" color="black.900">
                   Features
                 </Text>
               </Link>
             </ListItem>
             <ListItem>
-              <Link href="#">
+              <Link as={RouterLink} to="/pricing">
                 <Text size="textlg" color="black.900">
                   Pricing
                 </Text>
               </Link>
             </ListItem>
           </UnorderedList>
+          
         </Flex>
         <Flex>
-          <Button ml={{ base: "0px", sm: "56px" }} fontWeight={700} minW="88px" borderRadius="4px" fontSize="13px">
+          <Button
+            as={RouterLink}
+            to="/login"
+            ml={{ base: "0px", sm: "56px" }}
+            fontWeight={700}
+            minW="88px"
+            borderRadius="4px"
+            fontSize="13px"
+            color="#0o66"
+          >
             Login
           </Button>
-          <Button colorScheme="orange.400" ml={{ base: "0px", sm: "28px" }} fontWeight={700} minW="136px" borderRadius="4px" fontSize="13px">
+          <Button
+            as={RouterLink}
+            to="/signup"
+            colorScheme="orange"
+            ml={{ base: "0px", sm: "28px" }}
+            fontWeight={700}
+            minW="136px"
+            borderRadius="4px"
+            fontSize="13px"
+          >
             Get Started
           </Button>
         </Flex>
+        
       </Flex>
     );
   }
-  
