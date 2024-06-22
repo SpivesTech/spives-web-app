6c66d8e94dffc151ef2c2a98d9474d5bb259f7a3
Were Samson Bruno <werefast2000@gmail.com>
2024-06-22 13:53:26 +0000

    create Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/Hero';
+import CTASection from '../components/CTA Section';
+import Benefits from '../components/Benefits';
+import Contact from '../components/Contact Section';
+import Footer from '../components/Footer';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div>
+
+    <Navbar />
+    <HeroSection />
+    <CTASection />
+    <Benefits />
+    <Contact/>
+    <Footer />
+  </div>;
 }
 
 export default Home;
