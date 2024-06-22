fd8b63c3705349627b33405eb4a241ce9da3634a
boypaida12 <baddysackey5@gmail.com>
2024-06-22 23:59:43 +0000

    hero section layout for desktop fixed

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,100 +1,61 @@
 import React from 'react';
-import { Box, Flex, Heading, Text, Button, Image, VStack, HStack } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Image,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
-    <Box
-      backgroundImage="url('/images/HeroBG image.png')"
-      backgroundSize="cover"
-      backgroundPosition="center"
-      height="calc(100vh - 80px)" // Adjust based on your navbar height
+    <Flex
+      minHeight="95vh"
+      bgImg="url(/images/hero-bg.png)"
+      alignItems="center"
       position="relative"
+      _after={{
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: '#03033E',
+        opacity: 0.8,
+        zIndex: 1,
+      }}
     >
-      {/* Overlay */}
-      <Box
-        position="absolute"
-        top="0"
-        left="0"
-        right="0"
-        bottom="0"
-        bg="rgba(0, 0, 102, 0.2)" // Adjust the last value (0.7) for transparency
-        zIndex="1"
-      />
-      
-      <Flex
-        direction={{ base: 'column', md: 'row' }}
-        align="center"
-        justify="space-between"
-        h="100%"
-        p={8}
-        position="relative"
-        zIndex="2"
-      >
-        <VStack align="flex-start" spacing={6} maxW="600px">
-          <Heading
-            as="h1"
-            size="4xl"
-            color="white"
-            fontWeight="bold"
-            lineHeight="1"
-          >
-            get <br />
-            <Text as="span" color="orange.400">
-              ahead
-            </Text>
-          </Heading>
-          <Text fontSize="xl" color="white">
-            Streamline Your Football Club Management with Spives
+      <Box position="relative" zIndex={2}>
+        <Image src="/images/hero-title.svg" />
+        <Text
+          color="white"
+          textAlign="start"
+          ms="58px"
+          fontSize="30px"
+          fontWeight="semibold"
+          my={4}
+        >
+          Streamline Your Football Club <br /> Management with{' '}
+          <Text as="span" color="#FF9434">
+            Spives
           </Text>
-          <Button
-            as={RouterLink}
-            to="/signup"
-            size="lg"
-            colorScheme="orange"
-            fontWeight="bold"
-          >
+        </Text>
+        <Flex>
+          <Button textAlign="start" as={RouterLink} ms="58px">
             Get Started
           </Button>
-        </VStack>
-
-        <VStack align="flex-end" spacing={4}>
-          <Box
-            bg="white"
-            borderRadius="md"
-            p={4}
-            boxShadow="lg"
-          >
-            <VStack spacing={2} align="center">
-              <HStack>
-                <Image src="/path-to-club-icon.png" boxSize="24px" />
-                <Text fontWeight="bold" fontSize="3xl">89</Text>
-              </HStack>
-              <Text fontSize="sm">MATCHES PLAYED</Text>
-              <HStack spacing={4}>
-                <VStack>
-                  <Image src="/path-to-ball-icon.png" boxSize="16px" />
-                  <Text fontSize="sm">20</Text>
-                </VStack>
-                <VStack>
-                  <Image src="/path-to-jersey-icon.png" boxSize="16px" />
-                  <Text fontSize="sm">11</Text>
-                </VStack>
-                <VStack>
-                  <Image src="/path-to-whistle-icon.png" boxSize="16px" />
-                  <Text fontSize="sm">3</Text>
-                </VStack>
-                <VStack>
-                  <Image src="/path-to-stopwatch-icon.png" boxSize="16px" />
-                  <Text fontSize="sm">40</Text>
-                </VStack>
-              </HStack>
-            </VStack>
-          </Box>
-          <Image src="/images/SpivesBall.png" boxSize="250px" />
-        </VStack>
-      </Flex>
-    </Box>
+        </Flex>
+      </Box>
+      <Box mx="auto" zIndex={2} alignSelf="flex-end">
+        <Image src="/images/hero-stats.svg" mx="auto" />
+        <Image src="images/SpivesBall.png" width={500} />
+      </Box>
+    </Flex>
   );
 };
 
