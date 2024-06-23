4bc2cb5b3cd8b7ed44649a4619e2a761632d529d
Were Samson Bruno <werefast2000@gmail.com>
2024-06-23 00:27:39 +0000

    create signup route

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Box, VStack, FormControl, FormLabel, Input, Button, Heading, Text, Link } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
@@ -9,39 +11,45 @@ const Signup = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add signup logic here
+    console.log('Signup attempt with:', email, password, confirmPassword);
   };
 
   return (
-    <div className="signup-container">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-      <p>
-        Already have an account? <Link to="/login">Login</Link>
-      </p>
-    </div>
+    <Box maxWidth="400px" margin="auto" mt={8}>
+      <VStack spacing={4} align="stretch">
+        
+        <Card>
+          <CardHeader><Heading>Get Started</Heading>
+        </CardHeader>
+          <CardBody>
+          <form onSubmit={handleSubmit}>
+          <FormControl id="email" isRequired>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </FormControl>
+          <FormControl id="password" isRequired mt={4}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </FormControl>
+          <FormControl id="confirmPassword" isRequired mt={4}>
+            <FormLabel>Confirm Password</FormLabel>
+            <Input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          </FormControl>
+          <Button type="submit" colorScheme="blue" width="full" mt={4}>
+            Sign Up
+          </Button>
+        </form>
+          </CardBody>
+          <CardFooter><Text mt={4}>
+          Already have an account?{' '}
+          <Link as={RouterLink} to="/login" color="blue.500">
+            Login
+          </Link>
+        </Text></CardFooter>
+        </Card>
+        
+      </VStack>
+    </Box>
   );
 };
 
