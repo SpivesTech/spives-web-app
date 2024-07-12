0bed66ac4f78237f60ecc1c67a8c9c18401cc6f7
Xarri Jorge <kodinglife@gmail.com>
2024-07-13 00:10:32 +0200

    Position showing up with Static inputs

--- src/components/NewPlayerCard/index.js
+++ src/components/NewPlayerCard/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { Grid, Flex, Box, VStack, HStack, Text, Image, Badge } from "@chakra-ui/react";
 
-export const NewPlayerCard = () => {
+const Player = {
+    "name": "Agine Prosper",
+    "age": 15,
+    "image": "https://res.cloudinary.com/broomstick/image/upload/t_spives_watermark/v1713292025/talents/Agine_Prosper_lfnh2k.png",
+    "mainPosition": "Defense",
+    "positions": "CAM,CDM,LAM",
+    "height": "",
+    "weight": "53.4Kg",
+    "club": "Future Kids FC",
+    "guardian": "",
+    "coach": "Gideon",
+    "foot": "Right",
+    "id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
+    "nickname": "The Wall",
+    "summary": "The Wall: A towering center-back known for his commanding presence in defense and aerial dominance. With a knack for timely tackles and clearances, he's a reliable anchor for any team's backline. Off the pitch, he's a dedicated student with a passion for strategy, aiming to pursue a professional career while representing Nigeria and dreaming of playing for Arsenal in the Premier League."
+}
+
+export const NewPlayerCard = ({ player = Player }) => {
+    const positions = player.positions.split(',').map(position => position.trim())
     return (
         <Box
             borderWidth="1px"
@@ -15,27 +33,26 @@ export const NewPlayerCard = () => {
             <VStack align="stretch" >
                 <VStack justifyContent="space-between" p={2} backgroundImage={"/images/favicon2.png"} borderRadius={5} backgroundRepeat={'no-repeat'} backgroundSize={'cover'} backgroundPosition={'center center'} backgroundColor={'white'} >
                     <HStack justifyContent="space-between" w="100%">
-                        <Image src="/images/NaijaFlag.png" alt="Country flag" maxWidth={'50px'} />
-                        <Image src="/assets/spivesinc fc.png" alt="Logo" maxWidth={'50px'} />
+                        <Image src={player.country} alt="Country flag" maxWidth={'50px'} />
+                        <Image src={player.club} alt="Logo" maxWidth={'50px'} />
                     </HStack>
 
-                    <Image src="https://res.cloudinary.com/broomstick/image/upload/t_spives_watermark/v1713292025/talents/Agine_Prosper_lfnh2k.png" alt="Player" />
+                    <Image src={player.image} alt="Player" />
 
                 </VStack>
                 <Grid p={1} columnGap={1} templateColumns={'65% 1fr'} borderRadius={5} minHeight={'60px'} backgroundColor={'#000066'} >
                     <Flex direction={'column'} backgroundColor={'white'} borderRadius={5} justifyContent={'space-between'} padding={3} >
                         <Box textAlign={'left'}>
-                            <Text fontSize={'md'} as='b' >KidWonder1389</Text>
-                            <Text fontSize={'sm'}>CAM</Text>
+                            <Text fontSize={'md'} as='b'>{player.name}</Text>
+                            <Text fontSize={'sm'}>{player.mainPosition}</Text>
                             <Text fontSize={'sm'}>14</Text>
                             <Text fontSize={'sm'}>Left Footer</Text>
                         </Box>
                         <HStack justifyContent={'space-between'} alignItems={'center'} >
                             <Grid templateColumns={'1fr 1fr 1fr'} columnGap={1}>
-                                <Image src="/assets/Positions/CAM spives.png" alt="position1" boxSize={'30px'} />
-                                <Image src="/assets/Positions/CAM spives.png" alt="position1" boxSize={'30px'} />
-                                <Image src="/assets/Positions/CAM spives.png" alt="position1" boxSize={'30px'} />
-
+                                <Image src={`/assets/Positions/${positions[0]} spives.png`} alt="position1" boxSize={'30px'} />
+                                <Image src={`/assets/Positions/${positions[1]} spives.png`} alt="position1" boxSize={'30px'} />
+                                <Image src={`/assets/Positions/${positions[2]} spives.png`} alt="position1" boxSize={'30px'} />
                             </Grid>
                             <Grid templateRows={'repeat(2 1fr'}>
                                 <Text fontSize={'xs'}>10</Text>
